71ad08183be490ae9c8a06080958c452874bc376
runapal5 <runapal5@gmail.com>
2017-03-29 09:20:29 +0530

    Update PizzaOrderController.java

--- src/main/java/com/niit/devcapsule/controller/PizzaOrderController.java
+++ src/main/java/com/niit/devcapsule/controller/PizzaOrderController.java
@@ -85,8 +85,6 @@ public class PizzaOrderController {
 	
 	
 	   for(Pizza pizza : pizzaOrderedList){	
-		   
-		   
 	    	 Pizza pizzaObj = pizzaService.findById(pizza.getId());
 	    	 
 	    	 if(pizzaObj != null){
